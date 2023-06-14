c5d148d697906a509921c5feb7951492dc49b08e
NicoGonzz <juannicolas28@live.com>
2023-06-14 14:20:10 -0500

    Se inicia el e-commerce de Harry Potter

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-img',  /* Selector */
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent {
+
+}
