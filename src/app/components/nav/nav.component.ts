b21fb68fb170fc70d40d3e317b6d52d945e31110
NicoGonzz <juannicolas28@live.com>
2023-06-21 21:32:11 -0500

    Agrega el sideMenu al mobile y se mejora el responsive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+      activeMenu = false;
+
+      toggleMenu() {
+        this.activeMenu = !this.activeMenu;
+      }
 
 }
